03d895c855efd2f14a05c93710850511503d0ba5
carolina <ci.arayagonzalez@gmail.com>
2025-07-06 22:49:35 -0400

    feat(hook): add custom hook to filter context data by code and return matching item

---
+++ src/hooks/useCountryByCode.ts
@@ -0,0 +1,11 @@
+import useCountryContext from "../context/useCountryContext";
+
+export const useCountryByCode = (code: string | undefined) => {
+  const { countries } = useCountryContext();
+
+  if (!countries || !code) return null;
+  return (
+    countries.find((item) => item.code.toLowerCase() === code.toLowerCase()) ??
+    null
+  );
+};
