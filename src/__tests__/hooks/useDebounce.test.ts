c7785c7a49516a4cffd5209ca0bdf72017409364
carolina <ci.arayagonzalez@gmail.com>
2025-07-07 14:22:20 -0400

    test(utils): add unit tests for useDebounce hook

---
+++ src/__tests__/hooks/useDebounce.test.ts
@@ -0,0 +1,33 @@
+import { renderHook, act } from "@testing-library/react";
+import { useDebounce } from "../../hooks/useDebounce";
+
+describe("useDebounce Hook", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it("Debería eliminar el rebote del valor después del retraso especificado", () => {
+    const { result, rerender } = renderHook(
+      ({ value, delay }) => useDebounce(value, delay),
+      {
+        initialProps: { value: "initial", delay: 500 },
+      }
+    );
+    expect(result.current).toBe("initial"); //initial: debe devolver el valor inicial inmediatamente
+
+    rerender({ value: "updated", delay: 500 }); //Actualizamos valor antes de que expire el timeout
+
+    expect(result.current).toBe("initial"); //Todavia debe devolver el valor anterior , rebote en progreso
+
+    act(() => {
+      jest.advanceTimersByTime(500); // Avanza el tiempo de rebote
+    });
+
+    expect(result.current).toBe("updated"); //Ahora devuelve el nuevo valor rebotado
+  });
+});
